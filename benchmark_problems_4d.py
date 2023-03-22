b9a4b43f193955e4a5c274de429a92f51af36ae0
Gabriel Pereira <berkpereira@gmail.com>
2023-03-22 18:24:22 +0000

    added benchmark parameters for MC ratio episodes and no steps

--- benchmark_problems_4d.py
+++ benchmark_problems_4d.py
@@ -48,3 +48,15 @@ epsilon_no_steps = 5
 epsilon_train_params = np.linspace(0, 1, 21)
 
 epsilon_no_evaluations = 3000
+
+"""
+
+STUFF FOR CHOICE OF RATIO OF EPISODES TO SIZE OF STATE SPACE AND NUMBER OF POLICY IMPROVEMENT STEPS
+
+"""
+
+ratio_episodes_steps_epsilon = 0.2 # as per what was concluded in epsilon investigations
+ratio_episodes_steps_ratio_params = np.linspace(0.1, 1, 10) # 0.1, 0.2, ..., 0.9, 1.0
+ratio_episodes_steps_no_steps_params = np.linspace(1, 10, 10) # 1, 2, ..., 9, 10
+
+ratio_episodes_steps_no_evaluations = 3000
