d6901b60b51132fc5287056b0210951cd93185f8
Gabriel Pereira <berkpereira@gmail.com>
2023-04-01 11:48:50 +0100

    cretaed helper GUI

---
+++ design_obstacles.py
@@ -0,0 +1,74 @@
+import tkinter as tk
+
+class GridGUI(tk.Frame):
+    def __init__(self, grid_size, landing_zone):
+        super().__init__()
+        self.grid_size = grid_size
+        self.landing_zone = landing_zone
+        self.grid_markers = [[0] * grid_size for _ in range(grid_size)]
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.grid = tk.Canvas(self, width=400, height=400)
+        self.grid.pack()
+
+        self.reset_button = tk.Button(self, text="Reset", command=self.reset)
+        self.reset_button.pack()
+
+        # Draw grid lines
+        for i in range(self.grid_size):
+            x = i * 400 // self.grid_size
+            self.grid.create_line(x, 0, x, 400)
+            self.grid.create_line(0, x, 400, x)
+
+        # Draw landing zone marker
+        lz_x, lz_y = self.landing_zone
+        self.grid_markers[lz_x][lz_y] = 1
+        x0 = lz_x * 400 // self.grid_size
+        y0 = lz_y * 400 // self.grid_size
+        x1 = (lz_x + 1) * 400 // self.grid_size
+        y1 = (lz_y + 1) * 400 // self.grid_size
+        self.grid.create_rectangle(x0, y0, x1, y1, fill="green")
+
+        # Bind click event to grid
+        self.grid.bind("<Button-1>", self.mark)
+
+    def mark(self, event):
+        x = event.x * self.grid_size // 400
+        y = event.y * self.grid_size // 400
+        if self.grid_markers[x][y] == 0:
+            self.grid_markers[x][y] = 2
+            x0 = x * 400 // self.grid_size
+            y0 = y * 400 // self.grid_size
+            x1 = (x + 1) * 400 // self.grid_size
+            y1 = (y + 1) * 400 // self.grid_size
+            self.grid.create_rectangle(x0, y0, x1, y1, fill="red")
+
+    def reset(self):
+        self.grid.delete("all")
+        self.grid_markers = [[0] * self.grid_size for _ in range(self.grid_size)]
+        lz_x, lz_y = self.landing_zone
+        self.grid_markers[lz_x][lz_y] = 1
+        x0 = lz_x * 400 // self.grid_size
+        y0 = lz_y * 400 // self.grid_size
+        x1 = (lz_x + 1) * 400 // self.grid_size
+        y1 = (lz_y + 1) * 400 // self.grid_size
+        self.grid.create_rectangle(x0, y0, x1, y1, fill="green")
+        # Draw grid lines
+        for i in range(self.grid_size):
+            x = i * 400 // self.grid_size
+            self.grid.create_line(x, 0, x, 400)
+            self.grid.create_line(0, x, 400, x)
+
+if __name__ == "__main__":
+    # Inputs here.
+    grid_size = 10
+    landing_zone = [5, 3]
+    
+
+    root = tk.Tk()
+    root.title("GridGUI")
+    app = GridGUI(grid_size, landing_zone)
+    app.pack()
+    root.mainloop()
