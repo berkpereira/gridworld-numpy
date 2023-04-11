4b3a08708da36620a671b607cf48769ed5ae29dc
Gabriel Pereira <berkpereira@gmail.com>
2023-04-11 12:54:54 +0100

    added plots

--- animate_4d.py
+++ animate_4d.py
@@ -12,13 +12,13 @@ import fig_specs as fsp
 
 
 if __name__ == "__main__":
-    grid_size = 10
+    grid_size = 8
     ID = 3
     wind = 1
-    method = 'dp'
-
+    method = 'mc'
     # remember state format: altitude, heading, x, y
-    initial_state = np.array([grid_size * 2, 1, 8, 2])
+    initial_state = np.array([grid_size * 2, 2, 4, 4])
+
 
     if wind == 1:
         MDP_file = f'benchmark-problems/4d/{grid_size}{ID}_wind_1,0.p'
@@ -30,11 +30,11 @@ if __name__ == "__main__":
     policy_file = f'benchmark-policies/4d/{method}/{grid_size}{ID}_wind_0,9_policy_array.npy'
     policy = dp4.array_to_policy(np.load(policy_file), MDP)
     history = mc4.generate_episode(MDP, policy, initial_state=initial_state)
-    mc4.play_episode(MDP, policy, history, save = True, two_d = True, file_name=f'{fsp.fig_path}/4d-{method}-trajectory.pdf')
+    mc4.play_episode(MDP, policy, history, save = False, two_d = True, file_name=f'{fsp.fig_path}/4d-{method}-trajectory.pdf')
     plt.close()
 
 
     # Now find MIP solution to the same problem
     ampl = mip4.initialise_ampl()
     mip_history, actions, solve_time = mip4.mip_history_and_actions_from_mdp(MDP, initial_state[2:4], initial_state[1], ampl)
-    mc4.play_episode(MDP, None, mip_history, save = True, two_d = True, file_name = f'{fsp.fig_path}/4d-ip-trajectory.pdf')
+    mc4.play_episode(MDP, None, mip_history, save = False, two_d = True, file_name = f'{fsp.fig_path}/4d-ip-trajectory.pdf')
